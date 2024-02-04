0f7f4abd7557a0f3ce0c441052cce3a98b2c6aca
SushiFortune <r_maouko@live.concordia.ca>
2024-02-03 19:28:08 -0800

    Function isValidConfirmationNumber() implemented to check if the format entered is correct.

---
+++ CancelReservation.js
@@ -0,0 +1,15 @@
+function isValidConfirmationNumber(confNumber) {
+    const regex=/^[A-Z]{2}\d{9}$/;
+    const isValid=regex.test(confNumber);
+    if(!isValid) {
+        console.log("The format you have entered is invalid. Please try again.");
+        return false;
+    }
+
+   cancelReservation(confNumber);
+   return true;
+
+  }
+  
+
+  
