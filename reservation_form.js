2b0bb08010ca085e6398bf259f962fb848ada8bf
Gibraltar-47 <147777064+Gibraltar-47@users.noreply.github.com>
2024-02-03 23:07:37 -0500

    Add files via upload
    form now creates an object from inputs

--- reservation_form.js
+++ reservation_form.js
@@ -1,62 +1,102 @@
+import logo from './logo.svg';
+import './App.css';
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-//reference https://www.w3schools.com/react/react_forms.asp
 
-function show_available_cars(){
+    
+  
 
-}
+function App() {
+  const [reservation, setReservation] = useState({});
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+       setReservation(values=>({...values, [name]: value}))
+    }
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        alert(reservation);
+    }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
 
-function reservation_form(){
-    return(
         <form onSubmit={handleSubmit}>
 
-            <label for='first_name'>First name:</label>
-            <input 
-            name="first_name" 
-            value={inputs.username || ""} 
-            onChange={handleChange} 
-            type="text" 
-            placeholder='First name' 
-            id='first_name'/>
+        <label for='first_name'>First name:</label>
+        <input 
+        name="first_name" 
+        value={reservation.first_name || ""} 
+        onChange={handleChange} 
+        type="text" 
+        placeholder='First name' 
+        id='first_name'/>
 
-            <label for='last_name'>Last name:</label>
-            <input name="last_name" 
-            type="text" 
-            placeholder='Last name' 
-            id='last_name'/>
-            <br/>
+        <label for='last_name'>Last name:</label>
+        <input 
+        name="last_name" 
+        value={reservation.last_name || ""} 
+        onChange={handleChange} 
+        type="text" 
+        placeholder='Last name' 
+        id='last_name'/>
+        <br/>
 
-            <label for='email'>Email:</label>
-            <input name="email" 
-            type='text' 
-            placeholder='Email Address' 
-            id='email'/><br/>
+        <label for='email'>Email:</label>
+        <input 
+        name="email" 
+        value={reservation.email || ""} 
+        onChange={handleChange} 
+        type='text' 
+        placeholder='Email Address' 
+        id='email'/><br/>
 
 
-            <label for='start_reservation'>Beginning date of your reservation:</label>
-            <input 
-            name="start_reservation" 
-            type='date' 
-            placeholder='Beginning date of reservation' 
-            id='start_resveration'/><br/>
+        <label for='start_reservation'>Beginning date of your reservation:</label>
+        <input 
+        name="start_reservation" 
+        value={reservation.start_reservation || ""} 
+        onChange={handleChange} 
+        type='date' 
+        placeholder='Beginning date of reservation' 
+        id='start_resveration'/><br/>
 
-            <label for='end_reservation'>Ending date of your reservation:</label>
-            <input 
-            name="end_reservation" 
-            type='date' 
-            placeholder='Ending date of reservation' 
-            id='end_reservation'/><br/>
-            
-            <label for='type_car'>Choose your preferred type of car:</label><br/>
-            <select name='type_car' id='type_car'>
-            <option value="SUV">SUV</option>
-            <option value="Sedan">Sedan</option>
-            <option value="Semi-Tuck">Semi-Tuck</option>
-            <option value="Van">Van</option>
-            <option value="all" selected>All</option>
+        <label for='end_reservation'>Ending date of your reservation:</label>
+        <input 
+        name="end_reservation" 
+        value={reservation.end_reservation || ""} 
+        onChange={handleChange} 
+        type='date' 
+        placeholder='Ending date of reservation' 
+        id='end_reservation'/><br/>
+        
+        <label for='type_car'>Choose your preferred type of car:</label><br/>
+          <select name='type_car' id='type_car' value={reservation.type_car||""} onChange={handleChange}>
+          <option value="all">All</option>
+          <option value="SUV">SUV</option>
+          <option value="Sedan">Sedan</option>
+          <option value="Semi-Truck">Semi-Truck</option>
+          <option value="Van">Van</option>
           </select><br/>
-            <input type='submit'></input>
-        </form>
-    )
+        <input type='submit'></input>
+    </form>
+      </header>
+    </div>
+  );
 }
+
+export default App;
