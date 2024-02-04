27ea21340ebec79e21aa4199d8224ada7fde4b25
Christelle Charles <christelle7210@outlook.com>
2024-02-04 09:36:40 -0500

    implement the continuous integration using Node.js, webpack and github workflows

---
+++ webpack.prod.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './App/src/scripts/index.js',
+  output: {
+    path: path.resolve(__dirname, './App/dist'),
+    filename: 'bundle.js',
+  },
+  optimization:{
+    minimizer: [
+      new TerserPlugin(), 
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './App/src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css', 
+    }),
+  ],
+};
