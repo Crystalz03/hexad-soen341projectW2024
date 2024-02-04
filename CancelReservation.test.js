4a484956c5649fd42efd66789774813265c93e3b
SushiFortune <r_maouko@live.concordia.ca>
2024-02-03 18:41:50 -0800

    I added test for empty strings and special character inputs

--- CancelReservation.test.js
+++ CancelReservation.test.js
@@ -1,8 +1,18 @@
 const cancelReservation=require('CancelReservation.js');
-test('Should return true when when given a valid confirmation number to cancel a reservation',() => {
-  const result=cancelReservation('MR123456789'); 
-  expect(result).toBe(true);});
 
-test('Should return false when when given an invalid confirmation number to cancel a reservation',() => {
+test('Should return true when when given a valid confirmation number to cancel a reservation',() => {
+const result=cancelReservation('MR123456789'); 
+expect(result).toBe(true);});
+
+test('Should return false when given an confirmation number that does not exist in the database to cancel a reservation',() => {
 const result=cancelReservation('AB123456789'); 
 expect(result).toBe(false);});
+
+test('Should return false when it is an empty string',() => {
+const result=cancelReservation(''); 
+expect(result).toBe(false);});
+
+test('Should return false if special characters are given',() => {
+  const result = cancelReservation('!@#$');
+  expect(result).toBe(false);
+});
