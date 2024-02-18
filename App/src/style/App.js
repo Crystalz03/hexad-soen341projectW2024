7858f84d56b6f1d532e96de9c05b9853e35916f7
Sydney Campbell <sydcampbell@hotmail.com>
2024-02-18 15:15:54 -0500

    file changed for displaying pages

---
+++ App/src/style/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "./pages/home";
+import "./../style/styles.css";
+
+//Displaying home page (currently not displaying)
+
+function App() {
+  return <Home />;
+}
+
+export default App;
