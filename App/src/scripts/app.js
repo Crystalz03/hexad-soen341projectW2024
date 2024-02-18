1792a9f89ee6bdd50f0ee65f9733d6320cc8f958
Sydney Campbell <sydcampbell@hotmail.com>
2024-02-18 15:15:19 -0500

    modified

--- App/src/scripts/app.js
+++ App/src/scripts/app.js
@@ -1,15 +1,3 @@
-import React from "react";
-import Home from "./../pages/home";
-import "./../style/styles.css";
-
-//Displaying home page (currently not displaying)
-
-function AnotherApp() {
-  return <Home />;
-}
-
-export default AnotherApp;
-
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
 const swaggerUi = require("swagger-ui-express");
