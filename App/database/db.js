51d0907216d3fee519e46f162e0b6ac3b1ecd85b
Christelle Charles <christelle7210@outlook.com>
2024-02-20 21:46:21 -0500

    fix OpenHandles

--- App/database/db.js
+++ App/database/db.js
@@ -31,10 +31,17 @@ pool.connect()
     console.error('Error connecting to the database:', err);
   });
 
-const server = app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+  let server;
 
+  beforeAll(() => {
+    server = app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  });
+  
+  afterAll((done) => {
+    server.close(done);
+  });
 
 
 module.exports = pool;
