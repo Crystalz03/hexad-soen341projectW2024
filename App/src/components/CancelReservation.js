d847b9baa26a7f84c2beeb85f567853e6bb7da2d
SushiFortune <r_maouko@live.concordia.ca>
2024-03-08 09:14:55 -0800

    Implemented the front-end of CancelReservation

--- App/src/components/CancelReservation.js
+++ App/src/components/CancelReservation.js
@@ -1,26 +1,34 @@
 
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import './../style/Cancel.css';
+
 
 
 function CancelReservationForm() {
 
     const [confirmationNumber, setConfirmationNumber] = useState('');
+    const [error, setError] = useState(null); 
     const navigate = useNavigate();
     
     function isFormatValidConfirmationNumber(confirmationNumber) {
     const regex=/^[A-Z]{1}\d{9}$/;
     const isValid=regex.test(confirmationNumber);
     if(!isValid) {
-        console.log("The format you have entered is invalid. Please try again.");
+        setError("The format you have entered is invalid. Please try again.");
         return false;
     }
+
     return true;
 
     }
 
 
     const callAPI = async () => {
+      if (!isFormatValidConfirmationNumber(confirmationNumber)) {
+        return; // Do not proceed with API call if format is invalid
+    }
+
         try {
           const response = await fetch(`http://localhost:9000/reservationRoute/${confirmationNumber}`, {
             method: "DELETE",
@@ -35,7 +43,8 @@ function CancelReservationForm() {
           if (response.ok){
             console.log('Reservation successfully canceled');
            }
-           else {  console.error('Failed to cancel reservation:', response.statusText);}
+           else { setError(response.statusText);
+             console.error('Failed to cancel reservation:', response.statusText);}
         
         } catch (error) {
           setError(error.message);
