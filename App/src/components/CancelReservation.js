0709e9a8b856c758d1609e736c8beb20c8d38ecc
SushiFortune <r_maouko@live.concordia.ca>
2024-03-09 15:02:48 -0800

    CancelReservation

--- App/src/components/CancelReservation.js
+++ App/src/components/CancelReservation.js
@@ -1,89 +1,67 @@
-
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import './../style/Cancel.css';
 
-
-
 function CancelReservationForm() {
-
     const [confirmationNumber, setConfirmationNumber] = useState('');
     const [error, setError] = useState(null); 
     const navigate = useNavigate();
     
-    function isFormatValidConfirmationNumber(confirmationNumber) {
-    const regex=/^[A-Z]{1}\d{9}$/;
-    const isValid=regex.test(confirmationNumber);
-    if(!isValid) {
-        setError("The format you have entered is invalid. Please try again.");
-        return false;
-    }
-
-    return true;
-
-    }
-
-
-    const callAPI = async () => {
-      if (!isFormatValidConfirmationNumber(confirmationNumber)) {
-        return; 
-    }
-
-        try {
-          const response = await fetch(`http://localhost:9000/reservationRoute/${confirmationNumber}`, {
-            method: "DELETE",
-            headers: {
-              "Content-Type": "application/json",
-            },
-    
-          });
-    
-          const data = await response.json();
-    
-          if (response.ok){
-            console.log('Reservation successfully canceled');
-           }
-           else { setError(response.statusText);
-             console.error('Failed to cancel reservation:', response.statusText);}
-        
-        } catch (error) {
-          setError(error.message);
-          console.error(error);
+    const isFormatValidConfirmationNumber = (confirmationNumber) => {
+        const regex = /^[A-Z]{1}\d{9}$/;
+        const isValid = regex.test(confirmationNumber);
+        if (!isValid) {
+            setError("The format you have entered is invalid. Please try again.");
+            return false;
         }
-      };
-    
-      
-    const handleCancel = () => {
-      const confirmed = window.confirm("Are you sure you want to cancel this reservation?");
-      if (confirmed) {
-          callAPI();
-      }};
+        return true;
+    }
 
-    const handleSubmit = (e) => { 
-        e.preventDefault(); 
-         handleCancel();
-
-      };
-    
-      const handleChange = (e) => {
+    const handleChange = (e) => {
         setConfirmationNumber(e.target.value);
-      };
+    };
 
-return (
-    <form  onSubmit={handleSubmit}  action="CancelR">
-      <label> Confirmation Number:
-      <input
-            type="text"
-            value={confirmationNumber} 
-            placeholder="Enter Confirmation Number"
-            required
-            onChange={handleChange}
-          ></input>
-      </label>
-        
-        <button type="submit">Cancel</button>
-   
-    </form>
-  );
+    const handleSubmit = async (e) => { 
+        e.preventDefault(); 
+        if (!isFormatValidConfirmationNumber(confirmationNumber)) {
+            return;
+        }
+        try {
+            const response = await fetch(`http://localhost:9000/reservations/${confirmationNumber}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            });
+            const data = await response.json();
+            if (response.ok) {
+                console.log('Reservation successfully canceled');
+     
+            } else {
+                setError('Failed to cancel reservation. Please try again later.');
+                console.error('Failed to cancel reservation:', response.statusText);
+            }
+        } catch (error) {
+            setError('An error occurred while cancelling the reservation. Please try again later.');
+            console.error('Error cancelling reservation:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} action="CancelR">
+            <label> Confirmation Number:
+                <input
+                    type="text"
+                    value={confirmationNumber} 
+                    placeholder="Enter Confirmation Number"
+                    required
+                    onChange={handleChange}
+                />
+            </label>
+            {error && <p className="error">{error}</p>}
+            <button type="submit">Cancel</button>
+        </form>
+    );
 }
+
 export default CancelReservationForm;
