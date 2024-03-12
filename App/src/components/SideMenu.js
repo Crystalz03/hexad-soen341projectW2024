b0b05c5460836079433a6d7b83bb310ad9f84da4
Gibraltar-47 <malak.sefriou@gmail.com>
2024-03-11 22:50:40 -0400

    merge from main

--- App/src/components/SideMenu.js
+++ App/src/components/SideMenu.js
@@ -19,6 +19,7 @@ export default function SideMenu() {
               </li>
               <li className="nav-list-components-1">About Hexad</li>
               <li className="nav-list-components-1">Reserve</li>
+            <li className="nav-list-components-1"><Link to="/Reserve">Reserve</Link></li>
               <li className="nav-list-components-1">View/Modify</li>
             </ul>
             <div className="nav-divider"></div>
@@ -39,32 +40,6 @@ export default function SideMenu() {
             <div className="nav-divider"></div>
           </aside>
         </div>
-    
-    <div className="main">
-      <div className="general-structure">
-        <aside className="nav sticky">
-           <div className="company-name-nav all-caps"><Link to="/">hexad</Link></div>
-          <ul className="nav-list-1">
-            <li className="nav-list-components-1">
-              <Link to="/SignIn">Sign In</Link>/
-              <Link to="/SignUp">Sign Up</Link>
-            </li>
-            <li className="nav-list-components-1">About Hexad</li>
-            <li className="nav-list-components-1"><Link to="/Reserve">Reserve</Link></li>
-            <li className="nav-list-components-1">View/Modify</li>
-            <li className="nav-list-components-2"><Link to="/BrowseAccounts">View Accounts</Link></li>
-          </ul>
-          <div className="nav-divider"></div>
-          <ul className="nav-list-2">
-            <Link to="/Browse" className="link-style">
-              Browse Vehicles
-            </Link>
-            <li className="nav-list-components-2">Locations</li>
-            <li className="nav-list-components-2">Contact Us</li>
-            <li className="nav-list-components-2">My Account</li>
-          </ul>
-          <div className="nav-divider"></div>
-        </aside>
       </div>
     </div>
   );
