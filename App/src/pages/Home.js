6b62adce3d9296b5cfabe94170d415342b53dfa0
Imen <khezzarimen@gmail.com>
2024-03-11 21:34:10 -0400

    Merged main

--- App/src/pages/Home.js
+++ App/src/pages/Home.js
@@ -1,13 +1,5 @@
 import React from "react";
-
-
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import SideMenu from "../components/SideMenu";
-
-
 import { Link } from "react-router-dom";
-
 import "./../style/style.css";
 import BrowseAccounts from './BrowseAccounts';
 import Footer from "../components/Footer";
