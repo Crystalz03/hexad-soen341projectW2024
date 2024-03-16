f00677a27d0fe9f0d4600536d9de40df9ffd33d3
Imen <khezzarimen@gmail.com>
2024-03-15 23:51:33 -0400

    fix sign up

--- App/src/App.js
+++ App/src/App.js
@@ -20,35 +20,29 @@ import AdminInventory from "./pages/AdminInventory";
 
 function App() {
   const [apiResponse, setApiResponse] = useState("");
-
   const callAPI = () => {
     fetch("http://localhost:9000/testAPI")
-      .then((res) => res.text())
-      .then((res) => {
-        console.log(res);
-        setApiResponse(res);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  };
-
+      .then(res => res.text())
+      .then(res => {console.log(res); 
+        setApiResponse(res
+        )})
+      .catch(error => console.error('Error fetching data:', error));
+  }
   const callAPI2 = () => {
     fetch("http://localhost:9000/vehicles", {
-      method: "GET",
+      method: 'GET', 
     })
-      .then((data) => data.json())
-      .then((data) => {
-        console.log(data.vehicle[0][0]); // always keep data.vehicle[0] this will return you an arrray with all the vehilce
-        setApiResponse(
-          data // data.vehicle[0] = array of vehicles  -- data.vehicle[0][0] = 1st vehicle in the list -- data.vehicle[0][0].ID == ID of the first vehicle
-        );
-      })
-      .catch((error) => console.error(error));
+      .then(data => data.json())
+      .then(data => {console.log(data.vehicle[0][0]); // always keep data.vehicle[0] this will return you an arrray with all the vehilce
+        setApiResponse(data                              // data.vehicle[0] = array of vehicles  -- data.vehicle[0][0] = 1st vehicle in the list -- data.vehicle[0][0].ID == ID of the first vehicle
+        )})
+      .catch(error => console.error(error));
   };
-
   useEffect(() => {
     callAPI();
     callAPI2();
   }, []); // Empty dependency array means this effect runs once when the component mounts
+
 
   return (
     <Router>
