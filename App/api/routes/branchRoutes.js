264a43b1add45b13d0810a789636ebd5397bfd1d
yaro0 <93663497+yaro0@users.noreply.github.com>
2024-03-25 00:46:57 -0400

    Refactoring

--- App/api/routes/branchRoutes.js
+++ App/api/routes/branchRoutes.js
@@ -8,7 +8,7 @@ router.post('/branches', async (req, res) => {
     const { BranchName, LatitudeCoord, LongitudeCoord, ID, Address } = req.body;
   
     try {
-      const result = await pool.query`INSERT INTO Branch (BranchName, LatitudeCoord, LongitudeCoord, ID, Adress) VALUES (${BranchName}, ${LatitudeCoord}, ${LongitudeCoord}, ${ID}, ${Address})`;
+      const result = await pool.query`INSERT INTO Branch (BranchName, LatitudeCoord, LongitudeCoord, ID, Address) VALUES (${BranchName}, ${LatitudeCoord}, ${LongitudeCoord}, ${ID}, ${Address})`;
       res.status(201).json({ message: 'Branch created successfully', branch: req.body }); 
     } catch (error) {
       console.error('Error creating branch:', error);
@@ -52,7 +52,7 @@ router.put('/branches/:id', async (req, res) => {
   const { BranchName, LatitudeCoord, LongitudeCoord, Address } = req.body;
 
   try {
-    const result = await pool.query`UPDATE Branch SET BranchName = ${BranchName}, LatitudeCoord = ${LatitudeCoord}, LongitudeCoord = ${LongitudeCoord}, Adress = ${Address} WHERE ID = ${branchId}`;
+    const result = await pool.query`UPDATE Branch SET BranchName = ${BranchName}, LatitudeCoord = ${LatitudeCoord}, LongitudeCoord = ${LongitudeCoord}, Address = ${Address} WHERE ID = ${branchId}`;
     res.status(200).json({ message: 'Branch updated successfully',  branch: req.body }); 
   } catch (error) {
     console.error('Error updating Branch:', error);
