eee325280cb474b79a41d00f8c2c818ea4b3b590
Gibraltar-47 <malak.sefriou@gmail.com>
2024-04-10 19:46:17 -0400

    npm run test fixed

--- App/tests/db.Applications.test.js
+++ App/tests/db.Applications.test.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(applicationRoutes);
 
 const application = {
-    id: "123456789",
+    id: '100000000',
     category: 'Compact',
     color: 'black',
     damages: 'none',
@@ -25,20 +25,21 @@ const application = {
 }
 
 const dbapplication = {
-    id: '223456789',
-    category: 'Compact',
-    color: 'black',
-    damages: 'none',
-    make: 'Sedan',
-    model: 'Civic',
-    mileage: 10000,
-    year: 2024,
-    offerAmount: 25000,
-    firstName: 'John',
-    lastName: 'Doe',
-    email:'johnDoe@gmail.com',
-    phone: '1234567890',
-    message: ''
+    ID: '100000000',
+    Category: 'Compact',
+    Color: 'black',
+    Damages: 'none',
+    Make: 'Sedan',
+    Model: 'Civic',
+    Mileage: 10000,
+    Year: 2024,
+    OfferAmount: 25000,
+    FirstName: 'John',
+    LastName: 'Doe',
+    Email:'johnDoe@gmail.com',
+    Phone: '1234567890',
+    Message: ''
+
 }
 
 describe('Application Routes', () => {
@@ -78,14 +79,14 @@ describe('Application Routes', () => {
 
     //Retrieving an application by ID
     it('should retrieve an application by ID', async () => {
-        const response = await request(app).get(`/applications/223456789`);
+        const response = await request(app).get(`/applications/100000000`);
         expect(response.status).toBe(200);
         expect(response.body.application).toEqual(dbapplication);
     });
 
     //Deleting an application by ID
     it('should delete an application by ID', async () => {
-        const response = await request(app).delete(`/applications/223456789`);
+        const response = await request(app).delete(`/applications/100000000`);
         expect(response.status).toBe(200);
         expect(response.body.message).toBe('Application deleted successfully');
     });
